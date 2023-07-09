4e3a5fd5d2da97e46f07d5c5030938c5ba9726ad
Luciano Correia <luciano.correia@gmail.com>
2023-07-09 13:28:14 +0400

    add store

---
+++ database/sqlc/store.go
@@ -0,0 +1,43 @@
+package database
+
+import (
+	"database/sql"
+)
+
+// Store provides all functions to execute db queries and transactions
+type Store interface {
+	Querier
+}
+
+// SQLStore provides all functions to execute SQL queries and transactions
+type SQLStore struct {
+	*Queries
+	db *sql.DB
+}
+
+// NewStore creates a new store
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
+		Queries: New(db),
+		db:      db,
+	}
+}
+
+// execTx executes a function within a database transaction
+// func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
+// 	tx, err := store.db.BeginTx(ctx, nil)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	q := New(tx)
+// 	err = fn(q)
+// 	if err != nil {
+// 		if rbErr := tx.Rollback(); rbErr != nil {
+// 			return fmt.Errorf("tx error: %v, rb error: %v", err, rbErr)
+// 		}
+// 		return err
+// 	}
+
+// 	return tx.Commit()
+// }
